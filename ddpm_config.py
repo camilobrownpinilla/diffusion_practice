2432e7447ab5c932b623be9fb047e5bd0111a92b
camilobrownpinilla <camilobrownpinilla@gmail.com>
2024-06-12 09:28:27 -0400

    base class implementation and config

---
+++ ddpm_config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+import torch
+import os
+
+@dataclass
+class BaseConfig:
+    DEVICE = torch.device
+    DATASET = "Cifar-10"
+
+    root_log_dir = os.path.join("Logs_Checkpoints", "Inference")
+    root_checkpoint_dir = os.path.join("Logs_Checkpoints", "checkpoints")
+
+    log_dir = "version_0"
+    checkpoint_dir = "version_0"
+
+class TrainingConfig:
+    TIMESTEPS = 1000
+    IMG_SHAPE = (3, 32, 32)
+    NUM_EPOCHS = 800
+    BATCH_SIZE = 32
+    LR = 2e-4
+    NUM_WORKERS = 2
